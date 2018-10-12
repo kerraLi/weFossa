b63c2a53e320750b230366609ef1e604a3b44207
Administrator <admin@example.com>
2018-10-12 18:06:27 +0800

    update blog table style

--- resources/views/Web/Blog/index.blade.php
+++ resources/views/Web/Blog/index.blade.php
@@ -4,10 +4,18 @@
 
 
 @section('container')
-    <h1>博客首页</h1>
-    <h2><a href="{{ url('/blog/work') }}">早九晚五</a></h2>
-    <h2><a href="{{ url('/blog/tourism') }}">浪迹天涯</a></h2>
-    <h2><a href="{{ url('/blog/food') }}">吃吃喝喝</a></h2>
-    <h2><a href="{{ url('/blog/photo') }}">点点滴滴</a></h2>
-    <h2><a href="{{ url('/blog/about') }}">模范夫妻</a></h2>
+
+    <div style="text-align: center">
+    <table frame="hsides"
+           cellpadding="10"
+           border="0"
+           style="width: 30%;
+           margin:auto">
+            <td><a href="{{ url('/blog/work') }}">早九晚五</a></td>
+            <td><a href="{{ url('/blog/tourism') }}">浪迹天涯</a></td>
+            <td><a href="{{ url('/blog/food') }}">吃吃喝喝</a></td>
+            <td><a href="{{ url('/blog/photo') }}">点点滴滴</a></td>
+            <td><a href="{{ url('/blog/about') }}">模范夫妻</a></td>
+        </tr>
+    </table></div>
 @endsection
