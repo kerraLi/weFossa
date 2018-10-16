83d8e42b89ca10835dae9a604baf140f53ca1162
Administrator <admin@example.com>
2018-10-16 11:23:29 +0800

    add js and css

--- resources/views/Web/Blog/index.blade.php
+++ resources/views/Web/Blog/index.blade.php
@@ -4,18 +4,11 @@
 
 
 @section('container')
-
-    <div style="text-align: center">
-        <table frame="hsides"
-               cellpadding="10"
-               border="0"
-               style="width: 30%;
-           margin:auto">
-            <tr>
-                @foreach ($pages as $key=>$page)
-                    <td><a href="{{ url('/blog/'.$key) }}">{{ $page['name'] }}</a></td>
-                @endforeach
-            </tr>
-        </table>
-    </div>
+    <script src="js/jquery.min.js"></script>
+    <script>
+        window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>')
+    </script>
+    <script src="/js/plugin.js"></script>
+    <script src="/js/application.js"></script>
+    <script src="/js/insight.js"></script>
 @endsection
