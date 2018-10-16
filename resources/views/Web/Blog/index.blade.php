802dfbe9095c713fe6b858646b79a3d513e08757
Administrator <admin@example.com>
2018-10-16 11:30:03 +0800

    delete js and css

--- resources/views/Web/Blog/index.blade.php
+++ resources/views/Web/Blog/index.blade.php
@@ -4,11 +4,5 @@
 
 
 @section('container')
-    <script src="js/jquery.min.js"></script>
-    <script>
-        window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>')
-    </script>
-    <script src="/js/plugin.js"></script>
-    <script src="/js/application.js"></script>
-    <script src="/js/insight.js"></script>
+
 @endsection
